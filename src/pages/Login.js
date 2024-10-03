5f303ba692dab740add795a250181bf64c60201c
Akshat Jain <125379408+akshat1423@users.noreply.github.com>
2024-10-04 01:27:04 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,7 +11,7 @@ const Login = ({ setUserId }) => {
     const handleLogin = async (e) => {
         e.preventDefault();
     
-        const response = await fetch('http://localhost:8000/api/login/', {
+        const response = await fetch('https://akshatgooglehackathon.pythonanywhere.com//api/login/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
